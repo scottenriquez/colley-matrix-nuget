3c08435f1f957683e45b7908d7a87c6b5c6aaa0d
Scottie Enriquez <scottie@enriquez.io>
2017-12-23 19:42:57 -0600

    Game simulation progress

---
+++ src/ColleyMatrix/Model/Team.cs
@@ -0,0 +1,10 @@
+﻿namespace ColleyMatrix.Model
+{
+    public class Team
+    {
+        public int TeamId { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public float ColleyRating { get; set; }
+    }
+}
--- src/ColleyMatrix/Service/ColleyMatrixService.cs
+++ src/ColleyMatrix/Service/ColleyMatrixService.cs
@@ -1,4 +1,6 @@
-﻿using ColleyMatrix.Provider;
+﻿using System.Collections.Generic;
+using ColleyMatrix.Model;
+using ColleyMatrix.Provider;
 
 namespace ColleyMatrix.Service
 {
@@ -6,17 +8,35 @@ namespace ColleyMatrix.Service
     {
         private readonly IMatrixProvider _matrixProvider;
         private readonly IValidatorService _validatorService;
+        private readonly int _dimensions;
+        private readonly IList<Team> _teams;
         
         public ColleyMatrixService(IMatrixProvider matrixProvider, IValidatorService validatorService)
         {
             _matrixProvider = matrixProvider;
             _validatorService = validatorService;
+            _dimensions = _matrixProvider.GetDimensions();
+            _teams = new List<Team>();
+            for (int teamId = 0; teamId < _dimensions; teamId++)
+            {
+                _teams.Add(new Team()
+                {
+                    TeamId = teamId,
+                    Wins = 0,
+                    Losses = 0,
+                    ColleyRating = 1
+                });
+            }
         }
         
         public void SimulateGame(int winnerId, int loserId)
         {
             _validatorService.ValidateTeam(winnerId);
             _validatorService.ValidateTeam(loserId);
+            int gameCount = _matrixProvider.GetValue(winnerId, loserId);
+            _matrixProvider.SetValue(winnerId, loserId, gameCount - 1);
+            _matrixProvider.SetValue(loserId, winnerId, gameCount - 1);
+            //TODO: update ratings and wins/losses
         }
 
         public void Solve()
